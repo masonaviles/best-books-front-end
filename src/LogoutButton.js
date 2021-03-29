5e2bb107281905471079d8d90c5818b180a1e868
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-29 15:53:08 -0700

    make buttons use bootstrap buttons

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function LogoutButton() {
   const {
@@ -8,9 +10,9 @@ function LogoutButton() {
   } = useAuth0();
 
   return isAuthenticated && (
-    <button onClick={() => {
+    <Button onClick={() => {
       logout({ returnTo: window.location.origin });
-    }}>Log out</button>
+    }}>Log out</Button>
   );
 }
 

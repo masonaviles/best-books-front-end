aceff8af9012ff82501c7700e56d97051a16055d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-30 14:24:52 -0700

    installed axios, created a component for bestbooks and the bookcarousel to render if there are more than 0 books stored in the application

---
+++ src/BestBooks.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+import BooksCarousel from './BooksCarousel';
+
+class BestBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    }
+  }
+
+  getBooks = async (e) => {
+    e.preventDefault();
+    try {
+      const SERVER = process.env.REACT_APP_SERVER;
+      const books = await axios.get(`${SERVER}/books`, { params: { email: user.email } });
+
+      console.log(books.data);
+
+      this.setState({ books: books.data })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <BooksCarousel
+          books={this.state.books}
+        />
+
+      </>
+    )
+  }
+}
+
+export default BestBooks;
---
+++ src/BooksCarousel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class BooksCarousel extends React.Component {
+
+}
+
+export default BooksCarousel;

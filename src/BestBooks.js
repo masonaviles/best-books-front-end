861f24b04db6b9b57738edfca7e1b66201946eed
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-01 20:37:23 -0700

    fixed issues reported in netlify deployment build log

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -52,12 +52,13 @@ class BestBooks extends React.Component {
   handleDescription = (bookDescription) => this.setState({ bookDescription });
   handleStatus = (bookStatus) => {
     console.log('update modal', this.state.indexOfChosenBook);
-    this.setState({ bookStatus });};
+    this.setState({ bookStatus });
+  };
 
-  deleteItem = async(index) => {
+  deleteItem = async (index) => {
     // use axios to call our API to delete the book at the index specified
     const SERVER = 'http://localhost:3001';
-    const newBooks = await axios.delete(`${SERVER}/books/${index}`, {params: {email: this.props.auth0.user.email}});
+    const newBooks = await axios.delete(`${SERVER}/books/${index}`, { params: { email: this.props.auth0.user.email } });
     console.log('after delete success', newBooks.data);
 
     // update our books state with the array of books that are NOT deleted
@@ -73,7 +74,7 @@ class BestBooks extends React.Component {
     const SERVER = 'http://localhost:3001';
     const bookReader = await axios.post(`${SERVER}/books`, { email: this.props.auth0.user.email, name: this.state.bookName, description: this.state.bookDescription, status: this.state.bookStatus });
     // console.log('bookreader', bookReader);
-    this.setState({ books: bookReader.data, showModal: false});
+    this.setState({ books: bookReader.data, showModal: false });
     console.log('updated book?', this.state.books);
   }
 
@@ -83,22 +84,21 @@ class BestBooks extends React.Component {
     const chosenBook = this.state.books[index];
     this.setState({ chosenBook, indexOfChosenBook: index });
     this.setState({ displayUpdateModal: true });
-  
   }
 
-  replaceBook = async(e) => {
+  replaceBook = async (e) => {
     e.preventDefault();
     const SERVER = 'http://localhost:3001';
     const chosenBook = this.state.books[this.state.indexOfChosenBook];
-    const book = { 
+    const book = {
       name: chosenBook.name,
       description: chosenBook.description,
-      status: this.state.bookStatus };
-
+      status: this.state.bookStatus
+    };
 
     this.state.books.splice(this.state.indexOfChosenBook, 1, book);
 
-    const updatedBooksArray = await axios.put(`${SERVER}/books/${this.state.indexOfChosenBook}`, {email: this.props.auth0.user.email, book: book} );
+    const updatedBooksArray = await axios.put(`${SERVER}/books/${this.state.indexOfChosenBook}`, { email: this.props.auth0.user.email, book: book });
 
     console.log('after update new array:', updatedBooksArray.data);
     this.setState({ books: updatedBooksArray.data, displayUpdateModal: false });
